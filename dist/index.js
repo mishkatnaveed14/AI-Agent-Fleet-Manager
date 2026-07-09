2901cd7a44b122c20b81345c5a1187f2fcc15791
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-07-09 17:27:51 +0500

    done!

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,3 @@
-// // agent data bIue print
-// class Agentnew {
-//   protected id: string;
-//   protected name: string;
-//   public status: "Active" | "Idle" | "Queued" | "Failed" | "Offline";
-//   constructor(
-//     id: string,
-//     name: string,
-//     status: "Active" | "Idle" | "Queued" | "Failed" | "Offline",
-//   ) {
-//     this.id = id;
-//     this.name = name;
-//     this.status = status;
-//   }
-//   public getid(): string {
-//     return this.id;
-//   }
-// }
-// class DeveloperAgent extends Agentnew {
-//   constructor(
-//     id: string,
-//     name: string,
-//     status: "Active" | "Idle" | "Queued" | "Failed" | "Offline",
-//   ) {
-//     super(id, name, status);
-//   }
-// }
-// // fleet manager
-// class FleetManager {
-//   private agents: Agentnew[] = [];
-//   public addAgent(acceptagent: Agentnew): void {
-//     this.agents.push(acceptagent);
-//     this.renderAgents();
-//   }
-//   // assign task
-//   public assignTask(agentId: string, task: string): void {
-//     let foundagent = this.agents.find((a) => a.getid() === agentId);
-//     if (foundagent !== undefined) {
-//       foundagent.status = "Active";
-//       console.log(`Task '${task}' assigned. Agent status is now Active.`);
-//       this.renderAgents();
-//     } else {
-//       console.error(`Agent with ID '${agentId}' not found.`);
-//     }
-//   }
-// }
 class Agentnew {
     constructor(id, name, status) {
         this.task = "";
@@ -69,9 +23,6 @@ class DeveloperAgent extends Agentnew {
         super(id, name, status);
     }
 }
-// ============================================================
-// FLEET MANAGER
-// ============================================================
 class FleetManager {
     constructor() {
         this.agents = [];
